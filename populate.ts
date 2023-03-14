f18ee5c3a776c30ef6ab59fc26bd8a3b3e78c876
DexterMes <kabinbhandari33@gmail.com>
2023-03-14 16:17:10 +0545

    Update populate function

--- populate.ts
+++ populate.ts
@@ -4,29 +4,32 @@ const fs = require("fs");
 
 const prisma = new PrismaClient();
 
-// async function seedNames() {
-// 	const rl = readline.createInterface({
-// 		input: fs.createReadStream("e:/name.json"),
-// 		crlfDelay: Infinity,
-// 	});
+async function seedNames() {
+	const rl = readline.createInterface({
+		input: fs.createReadStream("e:/name.json"),
+		crlfDelay: Infinity,
+	});
 
-// 	for await (const line of rl) {
-// 		const name = JSON.parse(line);
-
-// 		await prisma.name.upsert({
-// 			where: { nconst: name.nconst },
-// 			update: {},
-// 			create: {
-// 				nconst: name.nconst,
-// 				primaryName: name.primaryName,
-// 				birthYear: name.birthYear,
-// 				deathYear: name.deathYear,
-// 				primaryProfession: { set: name.primaryProfession },
-// 				knownForTitles: { set: name.knownForTitles },
-// 			},
-// 		});
-// 	}
-// }
+	for await (const line of rl) {
+		const name = JSON.parse(line);
+		try {
+			await prisma.name.upsert({
+				where: { nconst: name.nconst },
+				update: {},
+				create: {
+					nconst: name.nconst,
+					primaryName: name.primaryName,
+					birthYear: name.birthYear,
+					deathYear: name.deathYear,
+					primaryProfession: { set: name.primaryProfession },
+					knownForTitles: { set: name.knownForTitles },
+				},
+			});
+		} catch (e) {
+			console.error(`Error upserting name: ${e}`);
+		}
+	}
+}
 
 // seedNames()
 // 	.catch((e) => console.error(e))
@@ -34,26 +37,29 @@ const prisma = new PrismaClient();
 // 		await prisma.$disconnect();
 // 	});
 
-// async function seedCrew() {
-// 	const rl = readline.createInterface({
-// 		input: fs.createReadStream("e:/crew.json"),
-// 		crlfDelay: Infinity,
-// 	});
+async function seedCrew() {
+	const rl = readline.createInterface({
+		input: fs.createReadStream("e:/crew.json"),
+		crlfDelay: Infinity,
+	});
 
-// 	for await (const line of rl) {
-// 		const crew = JSON.parse(line);
-
-// 		await prisma.crew.upsert({
-// 			where: { tconst: crew.tconst },
-// 			update: {},
-// 			create: {
-// 				tconst: crew.tconst,
-// 				directors: { set: crew.directors },
-// 				writers: { set: crew.writers },
-// 			},
-// 		});
-// 	}
-// }
+	for await (const line of rl) {
+		const crew = JSON.parse(line);
+		try {
+			await prisma.crew.upsert({
+				where: { tconst: crew.tconst },
+				update: {},
+				create: {
+					tconst: crew.tconst,
+					directors: { set: crew.directors },
+					writers: { set: crew.writers },
+				},
+			});
+		} catch (e) {
+			console.error(`Error upserting names: ${e}`);
+		}
+	}
+}
 
 // seedCrew()
 // 	.catch((e) => console.error(e))
@@ -68,54 +74,30 @@ const prisma = new PrismaClient();
 // 	});
 
 // 	for await (const line of rl) {
-// 		const principal = JSON.parse(line);
+// 		const principals = JSON.parse(line);
 
-// 		await prisma.principals.upsert({
-// 			where: { tconst_nconst: { tconst: principal.tconst, nconst: principal.nconst } },
-// 			update: {},
-// 			create: {
-// 				tconst: principal.tconst,
-// 				nconst: principal.nconst,
-// 				ordering: principal.ordering,
-// 				category: principal.category,
-// 				job: principal.job,
-// 				characters: { set: principal.characters },
-// 				name: { connect: { nconst: principal.nconst } },
-// 				movie: { connect: { tconst: principal.tconst } },
-// 			},
-// 		});
+// 		try {
+// 			await prisma.principals.upsert({
+// 				where: { tconst_nconst: { tconst: principals.tconst, nconst: principals.nconst } },
+// 				update: {},
+// 				create: {
+// 					tconst: principals.tconst,
+// 					ordering: principals.ordering,
+// 					nconst: principals.nconst,
+// 					category: principals.category,
+// 					job: principals.job,
+// 					characters: { set: principals.characters },
+// 				},
+// 			});
+// 		} catch (e) {
+// 			console.error(`Error upserting principals: ${e}`);
+// 		}
 // 	}
 // }
 
-async function seedPrincipals() {
-	const rl = readline.createInterface({
-		input: fs.createReadStream("e:/principals.json"),
-		crlfDelay: Infinity,
-	});
-
-	for await (const line of rl) {
-		const principals = JSON.parse(line);
-
-		try {
-			await prisma.principals.upsert({
-				where: { tconst_nconst: { tconst: principals.tconst, nconst: principals.nconst } },
-				update: {},
-				create: {
-					tconst: principals.tconst,
-					ordering: principals.ordering,
-					nconst: principals.nconst,
-					category: principals.category,
-					job: principals.job,
-					characters: { set: principals.characters },
-				},
-			});
-		} catch (e) {
-			console.error(`Error upserting principals: ${e}`);
-		}
-	}
-}
-
-seedPrincipals()
+seedCrew();
+seedNames()
+	// seedPrincipals()
 	.catch((e) => console.error(e))
 	.finally(async () => {
 		await prisma.$disconnect();
