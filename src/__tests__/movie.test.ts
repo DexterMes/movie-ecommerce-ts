417b6d0c11c493de1c24af6b1048961864b9e39e
DexterMes <kabinbhandari33@gmail.com>
2023-03-17 11:52:50 +0545

    Update filter function

--- src/__tests__/movie.test.ts
+++ src/__tests__/movie.test.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
-import { getAllMovies, getMoviesByGenre, getMoviesByName, getMoviesByPeople, getMoviesByYear } from "../movie.controller";
+import { getAllMovies, getFilteredMovies } from "../movie.controller";
 
 let mockRequest: Partial<Request>;
 let mockResponse: Partial<Response>;
 
 let token: string =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkthYmluIiwiaWF0IjoxNjc4NjE4NTUyLCJleHAiOjE2Nzg2NDM3NTJ9.3_CDLSmU3v8hAisdLek33D4KWtEqZr7Gq5O15JOa3kQ";
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlRlc3QxIiwiaWF0IjoxNjc5MDI3NTgxLCJleHAiOjE2NzkwNTI3ODF9.JjjRJZUqad24gjJ-F8bKMfilhKfdW2dtkY__pC_ZOFM";
 
 beforeEach(() => {
 	mockRequest = {};
@@ -27,101 +27,62 @@ test("Test getAllMovies to Pass", async () => {
 	}
 });
 
-test("Test getMoviesByYear to Pass", async () => {
+test("Test getFilteredMovies to Pass", async () => {
 	try {
 		const mockRequest = {
 			headers: { authorization: `Bearer ${token}` },
-			params: { year: "2000" },
+			query: { name: "Space-pigs" },
 		};
-		await getMoviesByYear(mockRequest as unknown as Request, mockResponse as Response, jest.fn());
+		await getFilteredMovies(mockRequest as unknown as Request, mockResponse as Response, jest.fn());
+		expect(mockResponse.json).toHaveBeenCalledWith({ movies: expect.any(Array), message: expect.any(String) });
+	} catch (error) {
+		fail(error);
+	}
+});
+test("Test getFilteredMovies to Pass", async () => {
+	try {
+		const mockRequest = {
+			headers: { authorization: `Bearer ${token}` },
+			query: { genre: "Comedy" },
+		};
+		await getFilteredMovies(mockRequest as unknown as Request, mockResponse as Response, jest.fn());
+		expect(mockResponse.json).toHaveBeenCalledWith({ movies: expect.any(Array), message: expect.any(String) });
+	} catch (error) {
+		fail(error);
+	}
+});
+test("Test getFilteredMovies to Pass", async () => {
+	try {
+		const mockRequest = {
+			headers: { authorization: `Bearer ${token}` },
+			query: { year: "2000" },
+		};
+		await getFilteredMovies(mockRequest as unknown as Request, mockResponse as Response, jest.fn());
+		expect(mockResponse.json).toHaveBeenCalledWith({ movies: expect.any(Array), message: expect.any(String) });
+	} catch (error) {
+		fail(error);
+	}
+});
+test("Test getFilteredMovies to Pass", async () => {
+	try {
+		const mockRequest = {
+			headers: { authorization: `Bearer ${token}` },
+			query: { people: "Ryan Reynolds" },
+		};
+		await getFilteredMovies(mockRequest as unknown as Request, mockResponse as Response, jest.fn());
 		expect(mockResponse.json).toHaveBeenCalledWith({ movies: expect.any(Array), message: expect.any(String) });
 	} catch (error) {
 		fail(error);
 	}
 });
 
-test("Test getMoviesByYear to Fail", async () => {
+test("Test getFilteredMovies to Fail", async () => {
 	try {
 		const mockRequest = {
 			headers: { authorization: `Bearer ${token}` },
-			params: { year: "00" },
+			query: { name: "test", genre: "test", year: "test", people: "test" },
 		};
-		await getMoviesByYear(mockRequest as unknown as Request, mockResponse as Response, jest.fn());
-	} catch (error) {
-		expect(mockResponse.json).toHaveBeenCalledWith({ error: expect.any(String) });
-	}
-});
-
-test("Test getMoviesByName to Pass", async () => {
-	try {
-		const mockRequest = {
-			headers: { authorization: `Bearer ${token}` },
-			params: { name: "Green Lantern" },
-		};
-		await getMoviesByName(mockRequest as unknown as Request, mockResponse as Response, jest.fn());
-		expect(mockResponse.json).toHaveBeenCalledWith({ movies: expect.any(Array), message: expect.any(String) });
-	} catch (error) {
-		fail(error);
-	}
-});
-
-test("Test getMoviesByName to Fail", async () => {
-	try {
-		const mockRequest = {
-			headers: { authorization: `Bearer ${token}` },
-			params: { year: "00" },
-		};
-		await getMoviesByName(mockRequest as unknown as Request, mockResponse as Response, jest.fn());
-	} catch (error) {
-		expect(mockResponse.json).toHaveBeenCalledWith({ error: expect.any(String) });
-	}
-});
-
-test("Test getMoviesByGenre to Pass", async () => {
-	try {
-		const mockRequest = {
-			headers: { authorization: `Bearer ${token}` },
-			params: { genre: "Comedy" },
-		};
-		await getMoviesByGenre(mockRequest as unknown as Request, mockResponse as Response, jest.fn());
-		expect(mockResponse.json).toHaveBeenCalledWith({ movies: expect.any(Array), message: expect.any(String) });
-	} catch (error) {
-		fail(error);
-	}
-});
-
-test("Test getMoviesByGenre to Fail", async () => {
-	try {
-		const mockRequest = {
-			headers: { authorization: `Bearer ${token}` },
-			params: { genre: "test" },
-		};
-		await getMoviesByGenre(mockRequest as unknown as Request, mockResponse as Response, jest.fn());
-	} catch (error) {
-		expect(mockResponse.json).toHaveBeenCalledWith({ error: expect.any(String) });
-	}
-});
-
-test("Test getMoviesByPeople to Pass", async () => {
-	try {
-		const mockRequest = {
-			headers: { authorization: `Bearer ${token}` },
-			params: { name: "Ryan Reynolds" },
-		};
-		await getMoviesByPeople(mockRequest as unknown as Request, mockResponse as Response, jest.fn());
-		expect(mockResponse.json).toHaveBeenCalledWith({ movies: expect.any(Array), message: expect.any(String) });
-	} catch (error) {
-		fail(error);
-	}
-});
-
-test("Test getMoviesByPeople to Fail", async () => {
-	try {
-		const mockRequest = {
-			headers: { authorization: `Bearer ${token}` },
-			params: { name: "test" },
-		};
-		await getMoviesByPeople(mockRequest as unknown as Request, mockResponse as Response, jest.fn());
+		await getFilteredMovies(mockRequest as unknown as Request, mockResponse as Response, jest.fn());
 	} catch (error) {
 		expect(mockResponse.json).toHaveBeenCalledWith({ error: expect.any(String) });
 	}
